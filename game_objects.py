c756be71cf6bc1b2dc710b73bd9037dff9dc6213
rusrap3547 <t3sserak@proton.me>
2025-12-12 23:25:37 -0500

    set up the dictionaries and added the 'sand' to the window

--- game_objects.py
+++ game_objects.py
@@ -1,16 +1,43 @@
-from asyncio import FrameCallGraphEntry
-
 import pygame
 import random
 import main
 
-class swimmingFish (self, race, color):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.race = race
-        self.color = color
-        self.speed = random.randint(1, 5)
+FISH_TYPES = [
+    {
+        "name": "bass",
+        "speed": 100,
+        "size": (40, 20),
+        "color": (34, 139, 34),
+        "spawn_rate": 0.5,
+        "points": 1
+    },
+    {
+        "name": "trout",
+        "speed": 150,
+        "size": (30, 15),
+        "color": (70, 130, 180),
+        "spawn_rate": 0.7,
+        "points": 2
+    },
+    {
+        "name": "salmon",
+        "speed": 200,
+        "size": (50, 25),
+        "color": (250, 128, 114),
+        "spawn_rate": 0.3,
+        "points": 5
+    }
+]
 
-        def move(self):
-            self.x = speed
+class Fish:
+    def __init__(self, fish_config):
+        self.name = fish_config["name"]
+        self.speed = fish_config["speed"]
+        self.size = fish_config["size"]
+        self.color = fish_config["color"]
+        self.points = fish_config["points"]
+        self.start = fish_config["spawn_rate"]
+
+# Usage:
+import random
+fish = Fish(random.choice(FISH_TYPES))
